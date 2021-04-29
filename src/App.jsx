9f1283d0ec7c2c529f959c0df0f4fe48efb499ad
leon <leonfrer@gmail.com>
2021-04-30 04:27:09 +0800

    update to class instead of function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ResponsiveLocalStorageLayout from "./GridComposer";
+import ResponsiveLayout from "./GridComposer";
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ResponsiveLocalStorageLayout />
+        <ResponsiveLayout />
       </div>
     );
   }
--- src/GridComposer.jsx
+++ src/GridComposer.jsx
@@ -10,148 +10,104 @@ import "./GridComposer.css";
 import BasicLine from "./chart/line/BasicLine";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
-const originalLayouts = getFromLS("layouts") || {};
 
-/**
- * This layout demonstrates how to sync multiple responsive layouts to localstorage.
- */
-export default function ResponsiveLocalStorageLayout() {
-  const [layouts, setLayouts] = React.useState(
-    JSON.parse(JSON.stringify(originalLayouts))
-  );
-  const [items, setItems] = React.useState(itemsTemplate);
-  const [newCounter, setNewCounter] = React.useState(0);
-  const [mounted, setMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    if (!mounted) {
-      setMounted(true);
-    }
-  }, [mounted, setMounted]);
-
-  // 通过item创建grid内容
-  // todo 需要修改
-  const createElement = (el) => {
-    const removeStyle = {
-      position: "absolute",
-      right: "2px",
-      top: 0,
-      cursor: "pointer",
+function generateLayout() {
+  return _.map(_.range(0, 10), function (item, i) {
+    var y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: Math.round(Math.random() * 5) * 2,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05,
     };
-    const i = el.i;
-    let content;
-    const testOption = {
-      xAxis: {
-        type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [
-        {
-          data: [150, 230, 224, 218, 135, 147, 200],
-          type: "line",
-        },
-      ],
+  });
+}
+
+export default class ResponsiveLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      layouts: { lg: generateLayout() },
+      mounted: false,
+      currentBreakpoint: "lg",
     };
-    if (el.chartKey === "BasicLine") {
-      content = <BasicLine option={testOption} w={el.w} />;
-    } else {
-      content = i;
-    }
-    console.log(el.isResizable);
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
+    this.onLayoutChange = this.onLayoutChange.bind(this);
+    this.generateDOM = this.generateDOM.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      mounted: true,
+    });
+  }
+
+  onBreakpointChange(breakpoint) {
+    this.setState({
+      currentBreakpoint: breakpoint,
+    });
+  }
+
+  onLayoutChange(layout, layouts) {
+    console.log("layout changed");
+  }
+
+  generateDOM() {
+    console.log("dom rerender");
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={l.static ? "static" : ""}>
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
+              Static - {i}
+            </span>
+          ) : (
+            <span className="text">{i}</span>
+          )}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    const cols = { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 };
 
     return (
-      <div key={i} data-grid={el}>
-        <span className="text">{content}</span>
-        <span className="remove" style={removeStyle}>
-          x
-        </span>
-      </div>
-    );
-  };
-
-  // 往items中添加元素
-  const onAddItem = () => {
-    console.log("add item");
-    setItems(
-      items.concat({
-        i: "n" + newCounter,
-        x: (items.length * 2) % 12,
-        y: Infinity,
-        w: 2,
-        h: 2,
-      })
-    );
-    setNewCounter(newCounter + 1);
-  };
-
-  // 重置布局
-  const resetLayout = () => {
-    setLayouts(originalLayouts);
-  };
-
-  const onLayoutChange = (layout, layouts) => {
-    // todo 保存到配置文件
-    saveToLS("layouts", layouts);
-    setLayouts({ layouts: layouts });
-  };
-
-  return (
-    <div>
       <div>
-        <div className="div-inline">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => resetLayout()}
-          >
-            重置模板
-          </Button>
+        <div>
+          {/* <div className="div-inline">
+            <Button variant="contained" color="primary" onClick={resetLayout}>
+              重置模板
+            </Button>
+          </div> */}
+          {/* <div className="div-inline">
+            <Button variant="contained" color="primary" onClick={onAddItem}>
+              添加模块
+            </Button>
+          </div> */}
+          <ItemSwitchDialog />
         </div>
-        <div className="div-inline">
-          <Button variant="contained" color="primary" onClick={onAddItem}>
-            添加模块
-          </Button>
-        </div>
-        <ItemSwitchDialog />
+
+        <ResponsiveReactGridLayout
+          className="layout"
+          cols={cols}
+          // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          rowHeight={30}
+          layouts={this.state.layouts}
+          useCSSTransforms={this.state.mounted}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={(layout, layouts) =>
+            this.onLayoutChange(layout, layouts)
+          }
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
       </div>
-
-      <ResponsiveReactGridLayout
-        className="layout"
-        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        rowHeight={30}
-        layouts={layouts}
-        useCSSTransforms={mounted}
-        onLayoutChange={(layout, layouts) => onLayoutChange(layout, layouts)}
-      >
-        {_.map(items, (el) => createElement(el))}
-      </ResponsiveReactGridLayout>
-    </div>
-  );
-}
-
-function getFromLS(key) {
-  let ls = {};
-  if (global.localStorage) {
-    try {
-      ls = JSON.parse(global.localStorage.getItem("rgl-8")) || {};
-    } catch (e) {
-      /*Ignore*/
-    }
-  }
-  return ls[key];
-}
-
-function saveToLS(key, value) {
-  if (global.localStorage) {
-    global.localStorage.setItem(
-      "rgl-8",
-      JSON.stringify({
-        [key]: value,
-      })
     );
   }
 }
